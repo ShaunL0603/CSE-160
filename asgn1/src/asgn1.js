11efbe9fc2fb53d47ce34fb5fe2ae12b207acef3
ShaunL0603 <shaun.lagon@gmail.com>
2026-04-07 18:03:44 -0700

    some code cleanup

--- asgn1/src/asgn1.js
+++ asgn1/src/asgn1.js
@@ -1,4 +1,4 @@
-// ColoredPoint.js (c) 2012 matsuda
+// ColoredPoint.js (c) 2012 matsuda Modified by Shaun Lagon, CSE 160
 // Vertex shader program
 var VSHADER_SOURCE =
   `
@@ -21,39 +21,20 @@ var FSHADER_SOURCE =
   }
   `;
 
+// Global variables
+var canvas;
+var gl;
+var a_Position;
+var u_FragColor;
+
 function main() {
-  // Retrieve <canvas> element
-  var canvas = document.getElementById('webgl');
-
-  // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
-  if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
-    return;
-  }
-
-  // Initialize shaders
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
-    return;
-  }
-
-  // // Get the storage location of a_Position
-  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
-    return;
-  }
-
-  // Get the storage location of u_FragColor
-  var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-  if (!u_FragColor) {
-    console.log('Failed to get the storage location of u_FragColor');
-    return;
-  }
+  // sets up canvas and gl variables
+  setupWebGL();
+  // set up GLSL shader programs and connect GLSL variables
+  connectVarialbesToGLSL();
 
   // Register function (event handler) to be called on a mouse press
-  canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position, u_FragColor) };
+  canvas.onmousedown = click;
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -62,16 +43,51 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
+function setupWebGL()
+{
+  // Retrieve <canvas> element
+  canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+}
+
+function connectVarialbesToGLSL() {
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // Get the storage location of a_Position
+  a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return;
+  }
+
+  // Get the storage location of u_FragColor
+  u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+  if (!u_FragColor) {
+    console.log('Failed to get the storage location of u_FragColor');
+    return;
+  }
+}
+
 var g_points = [];  // The array for the position of a mouse press
 var g_colors = [];  // The array to store the color of a point
-function click(ev, gl, canvas, a_Position, u_FragColor) {
+function click(ev) {
   var x = ev.clientX; // x coordinate of a mouse pointer
   var y = ev.clientY; // y coordinate of a mouse pointer
   var rect = ev.target.getBoundingClientRect();
-
+  
   x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
   y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
-
+  
   // Store the coordinates to g_points array
   g_points.push([x, y]);
   // Store the coordinates to g_points array
@@ -82,15 +98,15 @@ function click(ev, gl, canvas, a_Position, u_FragColor) {
   } else {                         // Others
     g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
   }
-
+  
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
-
+  
   var len = g_points.length;
   for(var i = 0; i < len; i++) {
     var xy = g_points[i];
     var rgba = g_colors[i];
-
+    
     // Pass the position of a point to a_Position variable
     gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
     // Pass the color of a point to u_FragColor variable
@@ -100,9 +116,8 @@ function click(ev, gl, canvas, a_Position, u_FragColor) {
   }
 }
 
-function clearRectangle()
-{
-    const ctx = document.getElementById('example').getContext('2d');
-    ctx.clearRect(-200, -200, 400, 400);
-    ctx.fillRect(-200, -200, 400, 400);
+function clearRectangle() {
+  const ctx = document.getElementById('example').getContext('2d');
+  ctx.clearRect(-200, -200, 400, 400);
+  ctx.fillRect(-200, -200, 400, 400);
 }
