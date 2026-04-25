9fe778a8175d385432c1fb6cf2e47236cc7580e7
ShaunL0603 <shaun.lagon@gmail.com>
2026-04-24 17:52:12 -0700

    Toes

--- asgn2/src/koala/koalaToes.js
+++ asgn2/src/koala/koalaToes.js
@@ -45,71 +45,70 @@ function makeKoalaToes(rootCubeMat, LEFT) {
     var firstToe = new Cube();
     firstToe.color = g_koalaGreyColorMat;
     firstToe.matrix = new Matrix4(thumbBone1Mat);
-    firstToe.matrix.translate(-0.2 * g_koalaScaleX,0.0 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
-    firstToe.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
+    firstToe.matrix.translate(-0.2 * g_koalaScaleX, -0.05 * g_koalaScaleY, -0.01 * g_koalaScaleZ);
+    firstToe.matrix.scale(0.5 * g_koalaScaleX, 0.2 * g_koalaScaleY, 0.45 * g_koalaScaleZ);
     firstToe.render();
 
     var firstToe2 = new Cube();
     firstToe2.color = g_koalaGreyColorMat;
     firstToe2.matrix = new Matrix4(thumbBone2Mat);
-    firstToe2.matrix.translate(-0.2 * g_koalaScaleX, 0.0 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
-    firstToe2.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
+    firstToe2.matrix.translate(-0.2 * g_koalaScaleX, -0.05 * g_koalaScaleY, 0.0 * g_koalaScaleZ);
+    firstToe2.matrix.scale(0.5 * g_koalaScaleX, 0.2 * g_koalaScaleY, 0.45 * g_koalaScaleZ);
     firstToe2.render();
 
     // SECOND TOE
     var fingyJoint2 = new Cube();
     fingyJoint2.color = g_koalaDarkerGreyColorMat;
     fingyJoint2.matrix = new Matrix4(rootCubeMat);
-    fingyJoint2.matrix.translate(0.5 * g_koalaScaleX, 0.1 * g_koalaScaleY, 1.0 * g_koalaScaleZ);
-    fingyJoint2.matrix.rotate(90, 1, 0, 0);
-    fingyJoint2.matrix.rotate(rotateToe2, 0, 1, 0);
+    fingyJoint2.matrix.translate(0.5 * g_koalaScaleX, 0.15 * g_koalaScaleY, 1.3 * g_koalaScaleZ);
+    fingyJoint2.matrix.rotate(rotateToe2, 0, 0, 1);
     var fingyJoint2Mat = new Matrix4(fingyJoint2.matrix);
     fingyJoint2.matrix.scale(g_koalaScaleX * 0.25, g_koalaScaleY * 0.125, g_koalaScaleZ * 0.125);
-    fingyJoint2.matrix.translate(-0.5, 0.0, -0.5);
+    fingyJoint2.matrix.translate(-0.5, -0.5, 0.0);
     fingyJoint2.render();
 
     var secondThumbBone1 = new Cube();
     secondThumbBone1.color = g_yellow;
     secondThumbBone1.matrix = new Matrix4(fingyJoint2Mat);
-    secondThumbBone1.matrix.translate(-0.07 * g_koalaScaleX, 0.03 * g_koalaScaleY, 0.06 * g_koalaScaleZ);
+    secondThumbBone1.matrix.translate(-0.06 * g_koalaScaleX, -0.46 * g_koalaScaleY, 0.03 * g_koalaScaleZ);
     var secondThumbBone1Mat = new Matrix4(secondThumbBone1.matrix);
-    secondThumbBone1.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.0625, g_koalaScaleZ * 0.4);
+    secondThumbBone1.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.4, g_koalaScaleZ * 0.0625);
     secondThumbBone1.render();
 
     var thumbJoint1 = new Cube();
     thumbJoint1.color = g_koalaDarkerGreyColorMat;
     thumbJoint1.matrix = new Matrix4(fingyJoint2Mat);
-    thumbJoint1.matrix.translate(0.0, 0.0, 0.5 * g_koalaScaleZ);
-    thumbJoint1.matrix.rotate(rotateToe2 * 1.5, 0, 1, 0);
+    thumbJoint1.matrix.translate(0.0, -0.47 * g_koalaScaleY, 0.0);
+    thumbJoint1.matrix.rotate(rotateToe2 * 1.3, 0, 0, 1);
     var thumbJoint1Mat = new Matrix4(thumbJoint1.matrix);
     thumbJoint1.matrix.scale(g_koalaScaleX * 0.25, g_koalaScaleY * 0.125, g_koalaScaleZ * 0.125);
-    thumbJoint1.matrix.translate(-0.5, 0.0, -0.5);
+    thumbJoint1.matrix.translate(-0.5, -0.5, 0.0);
     thumbJoint1.render();
 
     var secondThumbBone2 = new Cube();
     secondThumbBone2.color = g_yellow;
     secondThumbBone2.matrix = new Matrix4(thumbJoint1Mat);
-    secondThumbBone2.matrix.translate(-0.07 * g_koalaScaleX, 0.03 * g_koalaScaleY, 0.06 * g_koalaScaleZ);
+    secondThumbBone2.matrix.translate(-0.07 * g_koalaScaleX, -0.46 * g_koalaScaleY, 0.03 * g_koalaScaleZ);
     var secondThumbBone2Mat = new Matrix4(secondThumbBone2.matrix);
-    secondThumbBone2.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.0625, g_koalaScaleZ * 0.4);
+    secondThumbBone2.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.4, g_koalaScaleZ * 0.0625);
     secondThumbBone2.render();
 
     var thumbJoint2 = new Cube();
     thumbJoint2.color = g_koalaDarkerGreyColorMat;
     thumbJoint2.matrix = new Matrix4(thumbJoint1Mat);
-    thumbJoint2.matrix.translate(0.0, 0.0, 0.5 * g_koalaScaleZ);
-    thumbJoint2.matrix.rotate(rotateToe2 * 1.5, 0, 1, 0);
+    thumbJoint2.matrix.translate(0.0, -0.47 * g_koalaScaleY, 0.0);
+    thumbJoint2.matrix.rotate(rotateToe2 * 1.3, 0, 0, 1);
     var thumbJoint2Mat = new Matrix4(thumbJoint2.matrix);
     thumbJoint2.matrix.scale(g_koalaScaleX * 0.25, g_koalaScaleY * 0.125, g_koalaScaleZ * 0.125);
-    thumbJoint2.matrix.translate(-0.5, 0.0, -0.5);
+    thumbJoint2.matrix.translate(-0.5, -0.5, 0.0);
     thumbJoint2.render();
 
     var secondThumbBone3 = new Cube();
     secondThumbBone3.color = g_yellow;
     secondThumbBone3.matrix = new Matrix4(thumbJoint2Mat);
-    secondThumbBone3.matrix.translate(-0.07 * g_koalaScaleX, 0.03 * g_koalaScaleY, 0.06 * g_koalaScaleZ);
+    secondThumbBone3.matrix.translate(-0.07 * g_koalaScaleX, -0.46 * g_koalaScaleY, 0.03 * g_koalaScaleZ);
     var secondThumbBone3Mat = new Matrix4(secondThumbBone3.matrix);
-    secondThumbBone3.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.0625, g_koalaScaleZ * 0.4);
+    secondThumbBone3.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.4, g_koalaScaleZ * 0.0625);
     secondThumbBone3.render();
 
     var secondToe = new Cube();
@@ -132,13 +131,12 @@ function makeKoalaToes(rootCubeMat, LEFT) {
     secondToe3.matrix.translate(-0.2 * g_koalaScaleX, -0.01 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
     secondToe3.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
     secondToe3.render();
-
+    
     // THIRD TOE
     var fingyJoint3 = new Cube();
     fingyJoint3.color = g_koalaDarkerGreyColorMat;
     fingyJoint3.matrix = new Matrix4(rootCubeMat);
-    fingyJoint3.matrix.translate(0.5 * g_koalaScaleX, 0.15 * g_koalaScaleY, 1.3 * g_koalaScaleZ);
-    // fingyJoint3.matrix.rotate(-10, 0, 1, 0);
+    fingyJoint3.matrix.translate(0.5 * g_koalaScaleX, 0.1 * g_koalaScaleY, 1.0 * g_koalaScaleZ);
     fingyJoint3.matrix.rotate(rotateToe3, 0, 0, 1);
     var fingyJoint3Mat = new Matrix4(fingyJoint3.matrix);
     fingyJoint3.matrix.scale(g_koalaScaleX * 0.25, g_koalaScaleY * 0.125, g_koalaScaleZ * 0.125);
@@ -189,104 +187,104 @@ function makeKoalaToes(rootCubeMat, LEFT) {
     indexBone3.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.4, g_koalaScaleZ * 0.0625);
     indexBone3.render();
 
-    var thirdToe = new Cube();
-    thirdToe.color = g_koalaGreyColorMat;
-    thirdToe.matrix = new Matrix4(indexBone1Mat);
-    thirdToe.matrix.translate(-0.2 * g_koalaScaleX,0.0 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
-    thirdToe.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
-    thirdToe.render();
+    var thirdtoe = new Cube();
+    thirdtoe.color = g_koalaGreyColorMat;
+    thirdtoe.matrix = new Matrix4(indexBone1Mat);
+    thirdtoe.matrix.translate(-0.2 * g_koalaScaleX,0.0 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
+    thirdtoe.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
+    thirdtoe.render();
 
-    var thirdToe2 = new Cube();
-    thirdToe2.color = g_koalaGreyColorMat;
-    thirdToe2.matrix = new Matrix4(indexBone2Mat);
-    thirdToe2.matrix.translate(-0.2 * g_koalaScaleX, 0.0 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
-    thirdToe2.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
-    thirdToe2.render();
+    var thirdtoe2 = new Cube();
+    thirdtoe2.color = g_koalaGreyColorMat;
+    thirdtoe2.matrix = new Matrix4(indexBone2Mat);
+    thirdtoe2.matrix.translate(-0.2 * g_koalaScaleX, 0.0 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
+    thirdtoe2.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
+    thirdtoe2.render();
 
-    var thirdToe3 = new Cube();
-    thirdToe3.color = g_koalaGreyColorMat;
-    thirdToe3.matrix = new Matrix4(indexBone3Mat);
-    thirdToe3.matrix.translate(-0.2 * g_koalaScaleX, -0.01 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
-    thirdToe3.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
-    thirdToe3.render();
-
+    var thirdtoe3 = new Cube();
+    thirdtoe3.color = g_koalaGreyColorMat;
+    thirdtoe3.matrix = new Matrix4(indexBone3Mat);
+    thirdtoe3.matrix.translate(-0.2 * g_koalaScaleX, -0.01 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
+    thirdtoe3.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
+    thirdtoe3.render();
+    
     // FOURTH TOE
     var fingyJoint4 = new Cube();
     fingyJoint4.color = g_koalaDarkerGreyColorMat;
     fingyJoint4.matrix = new Matrix4(rootCubeMat);
-    fingyJoint4.matrix.translate(0.5 * g_koalaScaleX, 0.0 * g_koalaScaleY, 0.47 * g_koalaScaleZ);
-    // fingyJoint4.matrix.rotate(-10, 0, 1, 0);
-    fingyJoint4.matrix.rotate(rotateToe4, 0, 0, 1);
+    fingyJoint4.matrix.translate(0.5 * g_koalaScaleX, 0.01 * g_koalaScaleY, 0.47 * g_koalaScaleZ);
+    fingyJoint4.matrix.rotate(90, 1, 0, 0);
+    fingyJoint4.matrix.rotate(rotateToe4, 0, 1, 0);
     var fingyJoint4Mat = new Matrix4(fingyJoint4.matrix);
     fingyJoint4.matrix.scale(g_koalaScaleX * 0.25, g_koalaScaleY * 0.125, g_koalaScaleZ * 0.125);
-    fingyJoint4.matrix.translate(-0.5, -0.5, 0.0);
+    fingyJoint4.matrix.translate(-0.5, 0.0, -0.5);
     fingyJoint4.render();
 
     var middleBone1 = new Cube();
     middleBone1.color = g_yellow;
     middleBone1.matrix = new Matrix4(fingyJoint4Mat);
-    middleBone1.matrix.translate(-0.06 * g_koalaScaleX, -0.46 * g_koalaScaleY, 0.03 * g_koalaScaleZ);
+    middleBone1.matrix.translate(-0.07 * g_koalaScaleX, 0.03 * g_koalaScaleY, 0.06 * g_koalaScaleZ);
     var middleBone1Mat = new Matrix4(middleBone1.matrix);
-    middleBone1.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.4, g_koalaScaleZ * 0.0625);
+    middleBone1.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.0625, g_koalaScaleZ * 0.4);
     middleBone1.render();
 
     var middleJoint1 = new Cube();
     middleJoint1.color = g_koalaDarkerGreyColorMat;
     middleJoint1.matrix = new Matrix4(fingyJoint4Mat);
-    middleJoint1.matrix.translate(0.0, -0.47 * g_koalaScaleY, 0.0);
-    middleJoint1.matrix.rotate(rotateToe4 * 1.3, 0, 0, 1);
+    middleJoint1.matrix.translate(0.0, 0.0, 0.5 * g_koalaScaleZ);
+    middleJoint1.matrix.rotate(rotateToe4 * 1.5, 0, 1, 0);
     var middleJoint1Mat = new Matrix4(middleJoint1.matrix);
     middleJoint1.matrix.scale(g_koalaScaleX * 0.25, g_koalaScaleY * 0.125, g_koalaScaleZ * 0.125);
-    middleJoint1.matrix.translate(-0.5, -0.5, 0.0);
+    middleJoint1.matrix.translate(-0.5, 0.0, -0.5);
     middleJoint1.render();
 
     var middleBone2 = new Cube();
     middleBone2.color = g_yellow;
     middleBone2.matrix = new Matrix4(middleJoint1Mat);
-    middleBone2.matrix.translate(-0.07 * g_koalaScaleX, -0.46 * g_koalaScaleY, 0.03 * g_koalaScaleZ);
+    middleBone2.matrix.translate(-0.07 * g_koalaScaleX, 0.03 * g_koalaScaleY, 0.06 * g_koalaScaleZ);
     var middleBone2Mat = new Matrix4(middleBone2.matrix);
-    middleBone2.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.4, g_koalaScaleZ * 0.0625);
+    middleBone2.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.0625, g_koalaScaleZ * 0.4);
     middleBone2.render();
 
     var middleJoint2 = new Cube();
     middleJoint2.color = g_koalaDarkerGreyColorMat;
     middleJoint2.matrix = new Matrix4(middleJoint1Mat);
-    middleJoint2.matrix.translate(0.0, -0.47 * g_koalaScaleY, 0.0);
-    middleJoint2.matrix.rotate(rotateToe4 * 1.3, 0, 0, 1);
+    middleJoint2.matrix.translate(0.0, 0.0, 0.5 * g_koalaScaleZ);
+    middleJoint2.matrix.rotate(rotateToe4 * 1.5, 0, 1, 0);
     var middleJoint2Mat = new Matrix4(middleJoint2.matrix);
     middleJoint2.matrix.scale(g_koalaScaleX * 0.25, g_koalaScaleY * 0.125, g_koalaScaleZ * 0.125);
-    middleJoint2.matrix.translate(-0.5, -0.5, 0.0);
+    middleJoint2.matrix.translate(-0.5, 0.0, -0.5);
     middleJoint2.render();
 
     var middleBone3 = new Cube();
     middleBone3.color = g_yellow;
     middleBone3.matrix = new Matrix4(middleJoint2Mat);
-    middleBone3.matrix.translate(-0.07 * g_koalaScaleX, -0.46 * g_koalaScaleY, 0.03 * g_koalaScaleZ);
+    middleBone3.matrix.translate(-0.07 * g_koalaScaleX, 0.03 * g_koalaScaleY, 0.06 * g_koalaScaleZ);
     var middleBone3Mat = new Matrix4(middleBone3.matrix);
-    middleBone3.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.4, g_koalaScaleZ * 0.0625);
+    middleBone3.matrix.scale(g_koalaScaleX * 0.125, g_koalaScaleY * 0.0625, g_koalaScaleZ * 0.4);
     middleBone3.render();
 
     var fourthToe = new Cube();
     fourthToe.color = g_koalaGreyColorMat;
     fourthToe.matrix = new Matrix4(middleBone1Mat);
-    fourthToe.matrix.translate(-0.2 * g_koalaScaleX,0.0 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
-    fourthToe.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
+    fourthToe.matrix.translate(-0.2 * g_koalaScaleX, -0.065 * g_koalaScaleY, -0.01 * g_koalaScaleZ);
+    fourthToe.matrix.scale(0.5 * g_koalaScaleX, 0.2 * g_koalaScaleY, 0.5 * g_koalaScaleZ);
     fourthToe.render();
 
     var fourthToe2 = new Cube();
     fourthToe2.color = g_koalaGreyColorMat;
     fourthToe2.matrix = new Matrix4(middleBone2Mat);
-    fourthToe2.matrix.translate(-0.2 * g_koalaScaleX, 0.0 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
-    fourthToe2.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
+    fourthToe2.matrix.translate(-0.2 * g_koalaScaleX, -0.065 * g_koalaScaleY, 0.01 * g_koalaScaleZ);
+    fourthToe2.matrix.scale(0.5 * g_koalaScaleX, 0.2 * g_koalaScaleY, 0.5 * g_koalaScaleZ);
     fourthToe2.render();
 
     var fourthToe3 = new Cube();
     fourthToe3.color = g_koalaGreyColorMat;
     fourthToe3.matrix = new Matrix4(middleBone3Mat);
-    fourthToe3.matrix.translate(-0.2 * g_koalaScaleX, -0.01 * g_koalaScaleY, -0.065 * g_koalaScaleZ);
-    fourthToe3.matrix.scale(0.5 * g_koalaScaleX, 0.45 * g_koalaScaleY, 0.2 * g_koalaScaleZ);
+    fourthToe3.matrix.translate(-0.2 * g_koalaScaleX, -0.065 * g_koalaScaleY, 0.02 * g_koalaScaleZ);
+    fourthToe3.matrix.scale(0.5 * g_koalaScaleX, 0.2 * g_koalaScaleY, 0.5 * g_koalaScaleZ);
     fourthToe3.render();
-
+    
     // FIFTH TOE
     var fingyJoint5 = new Cube();
     fingyJoint5.color = g_koalaDarkerGreyColorMat;
