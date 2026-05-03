07f28d8c751245ea4977d149786279c0460d4b65
ShaunL0603 <shaun.lagon@gmail.com>
2026-05-03 15:17:11 -0700

    UVs fixed

--- asgn3/src/asgn3.js
+++ asgn3/src/asgn3.js
@@ -34,7 +34,7 @@ var FSHADER_SOURCE =
   void main() {
     gl_FragColor = u_FragColor;
     gl_FragColor = vec4(v_UV, 1.0, 1.0);
-    // gl_FragColor = texture2D(u_Sampler0, v_UV);
+    gl_FragColor = texture2D(u_Sampler0, v_UV);
   }
   `;
 
@@ -49,6 +49,7 @@ var u_ProjectionMatrix;
 var u_ViewMatrix;
 var u_GlobalRotationMatrix;
 var u_FragColor;
+var u_Sampler0;
 
 let g_startTime = performance.now() / 1000.0;
 let g_seconds = performance.now() / 1000.0 - g_startTime;
@@ -75,6 +76,8 @@ function main() {
 
   document.addEventListener("keydown", (ev) => { updateKeyDown(ev); });
   document.addEventListener("keyup", (ev) => { updateKeyUp(ev); });
+
+  initTextures(0);
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   requestAnimationFrame(tick);
@@ -123,7 +126,7 @@ function connectVariablesToGLSL() {
     return -1;
   }
   
-  // Get the storage location of a_Position
+  // Get the storage locations
   a_Position = gl.getAttribLocation(gl.program, "a_Position");
   if (a_Position < 0) {
     console.log("Failed to get the storage location of a_Position");
@@ -165,6 +168,53 @@ function connectVariablesToGLSL() {
     console.log("Failed to get the storage location of u_ViewMatrix");
     return -1;
   }
+
+  // Get the storage location of u_Sampler
+  var u_Sampler0 = gl.getUniformLocation(gl.program, 'u_Sampler0');
+  if (!u_Sampler0) {
+    console.log('Failed to get the storage location of u_Sampler0');
+    return false;
+  }
+}
+
+function initTextures(n) {
+  var image = new Image();  // Create the image object
+  if (!image) {
+    console.log('Failed to create the image object');
+    return false;
+  }
+  // Register the event handler to be called on loading an image
+  image.onload = function() { loadTexture(n, u_Sampler0, image); };
+  // Tell the browser to load an image
+  image.src = 'assets/sky.jpg';
+
+  return true;
+}
+
+function loadTexture(n, u_Sampler, image) {
+  var texture = gl.createTexture();   // Create a texture object
+  if (!texture) {
+    console.log('Failed to create the texture object');
+    return false;
+  }
+  
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+  // Enable texture unit0
+  gl.activeTexture(gl.TEXTURE0);
+  // Bind the texture object to the target
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Set the texture parameters
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  // Set the texture image
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  
+  // Set the texture unit 0 to the sampler
+  gl.uniform1i(u_Sampler, 0);
+  
+  gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
+
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
 }
 
 var g_camera = new Camera();
