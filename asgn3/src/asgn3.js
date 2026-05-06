6fb014bb3ef0836cc502d9ea3606b705383388a3
ShaunL0603 <shaun.lagon@gmail.com>
2026-05-06 11:06:14 -0700

    moved more stuff

--- asgn3/src/asgn3.js
+++ asgn3/src/asgn3.js
@@ -267,29 +267,3 @@ function tick() {
     };
     requestAnimationFrame(tick);
 }
-
-function placeBlock(closestDistance) {
-    let origin = new Vector3(g_camera.eye.elements);
-    let direction = new Vector3();
-    direction.set(g_camera.at);
-    direction.sub(origin);
-    direction.normalize();
-
-    let spawnDistance = closestDistance;
-    direction.mul(spawnDistance);
-
-    let hitPoint = new Vector3(origin.elements);
-    hitPoint.add(direction);
-
-    let newCube = new Cube();
-    newCube.type = "block";
-    newCube.color = [0.0, 0.0, 0.0, 1.0];
-    newCube.textureNum = -2;
-    newCube.matrix.translate(
-        hitPoint.elements[0] - 0.25,
-        hitPoint.elements[1],
-        hitPoint.elements[2] - 0.25
-    );
-    newCube.matrix.scale(0.5, 0.5, 0.5);
-    g_worldObjs.push(newCube);
-}
